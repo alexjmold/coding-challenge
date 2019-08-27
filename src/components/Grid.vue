a000b3ac30d8c434d63c9a1a3aec662870e2fbc8
Alex Mold <alexmold@TM.local>
2019-08-27 11:07:29 +0100

    formatting updated

--- src/components/Grid.vue
+++ src/components/Grid.vue
@@ -1,61 +1,55 @@
 <template>
-	<div class="grid-container">
-		<div
-			class="row"
-			v-for="index in rows"
-			:key="index"
-			:style="{
+  <div class="grid-container">
+    <div
+      class="row"
+      v-for="index in rows"
+      :key="index"
+      :style="{
 				width: `${gridItemSize * columns}px`,
 				height: `${gridItemSize}px`
 			}"
-		>
-			<div
-				class="column"
-				v-for="index in columns"
-				:key="index"
-				:style="{
+    >
+      <div
+        class="column"
+        v-for="index in columns"
+        :key="index"
+        :style="{
 					width: `${gridItemSize}px`,
 					height: `${gridItemSize}px`
 				}"
-			>
-			</div>
-		</div>
-	</div>
+      ></div>
+    </div>
+  </div>
 </template>
 
 <script>
-	export default {
-		props: [
-			'columns',
-			'rows',
-			'gridItemSize',
-		],
-		data() {
-			return {};
-		},
-	}
+export default {
+  props: ["columns", "rows", "gridItemSize"],
+  data() {
+    return {};
+  }
+};
 </script>
 
 <style lang="scss">
 .grid-container {
-	margin: 0px auto;
-	position: relative;
-	width: auto;
-	border: 1px solid #1B998B;
-	box-sizing: border-box;
+  margin: 0px auto;
+  position: relative;
+  width: auto;
+  border: 1px solid #1b998b;
+  box-sizing: border-box;
 }
 
 .row {
-	margin: 0;
-	padding: 0;
+  margin: 0;
+  padding: 0;
 }
 
 .column {
-	border: 1px solid #1B998B;
-	display: inline-block;
-	margin: 0;
-	padding: 0;
-	box-sizing: border-box;
+  border: 1px solid #1b998b;
+  display: inline-block;
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
 }
-
 </style>
